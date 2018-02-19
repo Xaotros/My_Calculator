37ed700abaa09dc3f4a92ed5e602a97837c8dc7e
Marko Nimac <nimac.terminator@hotmail.com>
2018-02-19 21:38:50 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace My_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.Location = new System.Drawing.Point(12, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = " Enter Here";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.TextBox1_Enter);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            this.textBox1.Leave += new System.EventHandler(this.TextBox1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(106, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Izračunaj";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox2.Location = new System.Drawing.Point(12, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(268, 23);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(52, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 42);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(201, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ver 1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(293, 284);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Form1.EvalExpression(textBox1.Text.ToCharArray()).ToString();
+        }
+
+        public static double EvalExpression(char[] expr)
+        {
+            return ParseSummands(expr, 0);
+        }
+
+        //rasclanjujem po '+', '-'
+        private static double ParseSummands(char[] expr, int index)
+        {
+            double x = ParseFactors(expr, ref index);
+            while (true)
+            {
+                char op = expr[index];
+                if (op != '+' && op != '-')
+                    return x;
+                index++;
+                double y = ParseFactors(expr, ref index);
+                if (op == '+')
+                    x += y;
+                else
+                    x -= y;
+            }
+        }
+
+        //rasclanjujem po '/', '*'
+        private static double ParseFactors(char[] expr, ref int index)
+        {
+            double x = GetDouble(expr, ref index);
+            while (true)
+            {
+                char op = expr[index];
+                if (op != '/' && op != '*')
+                    return x;
+                index++;
+                double y = GetDouble(expr, ref index);
+                if (op == '/')
+                    x /= y;
+                else
+                    x *= y;
+            }
+        }
+
+        //metoda koja vraca double iz stringa
+        private static double GetDouble(char[] expr, ref int index)
+        {
+            string dbl = "";
+            while (((int)expr[index] >= 48 && (int)expr[index] <= 57) || expr[index] == '.' || expr[index] == ',')
+            {
+                if (expr[index] == '.')
+                    expr[index] = ',';
+
+                dbl = dbl + expr[index].ToString();
+                index++;
+
+                if (index == expr.Length)
+                {
+                    index--;
+                    break;
+                }
+            }
+            return double.Parse(dbl);
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            //omogucit unos samo brojki i operatora + ostali simboli
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 46 && ch != '/' && ch != '*' && ch != 13 && ch != '+'
+                && ch != '-' && ch != '(' && ch != ')' && ch != '.' && ch != ',' && ch != 27)
+            {
+                e.Handled = true;
+                MessageBox.Show("Please enter valid value");
+            }
+            //enter key kao button klick
+            else if (ch == 13)
+            {
+                textBox2.Text = Form1.EvalExpression(textBox1.Text.ToCharArray()).ToString();
+            //da ne cujemo 'ding' nakon entera
+                e.Handled = true;
+              
+            }
+            //pa zasto ne i esc ubacit
+            else if (ch == 27)
+            {  
+              this.Close();
+            }
+        }
+
+
+        private void TextBox1_Enter(object sender, EventArgs e)
+        {
+            //PlaceHolder za enter
+            if (textBox1.Text == " Enter Here")
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        private void TextBox1_Leave(object sender, EventArgs e)
+        {
+            //PlaceHolder za leave
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = " Enter Here";
+            }
+        }
+    }
+}
